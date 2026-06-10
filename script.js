fefcdc44d0ff64f8b5adbc7d2949ba298e12fb98
thedigioverse-code <thedigioverse@gmail.com>
2026-06-10 20:15:35 +0500

    Fix hCaptcha sitekey typo and add custom CSS to center and scale the widget on mobile devices

--- script.js
+++ script.js
@@ -842,7 +842,7 @@ if (document.readyState === 'loading') {
         captchaElements.forEach(function (el) {
           if (!el.getAttribute('data-hcaptcha-id')) {
             var widgetId = hcaptcha.render(el, {
-              sitekey: el.getAttribute('data-sitekey') || '10000000-ffff-ffff-ffff-ffffffffffff'
+              sitekey: el.getAttribute('data-sitekey') || '10000000-ffff-ffff-ffff-000000000001'
             });
             el.setAttribute('data-hcaptcha-id', widgetId);
           }
@@ -919,7 +919,7 @@ if (document.readyState === 'loading') {
         if (submitBtn && !form.querySelector('.h-captcha')) {
           var captchaDiv = document.createElement('div');
           captchaDiv.className = 'h-captcha';
-          captchaDiv.setAttribute('data-sitekey', '10000000-ffff-ffff-ffff-ffffffffffff');
+          captchaDiv.setAttribute('data-sitekey', '10000000-ffff-ffff-ffff-000000000001');
           captchaDiv.style.display = 'flex';
           captchaDiv.style.justifyContent = 'center';
           captchaDiv.style.marginBottom = '20px';
@@ -930,7 +930,7 @@ if (document.readyState === 'loading') {
           // If hCaptcha library is already loaded, render it immediately
           if (typeof hcaptcha !== 'undefined') {
             var widgetId = hcaptcha.render(captchaDiv, {
-              sitekey: '10000000-ffff-ffff-ffff-ffffffffffff'
+              sitekey: '10000000-ffff-ffff-ffff-000000000001'
             });
             captchaDiv.setAttribute('data-hcaptcha-id', widgetId);
           }
