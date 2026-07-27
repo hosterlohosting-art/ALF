ec6c99b4a0a61734aa417c934a56ddc2ffe6e5f3
thedigioverse-code <thedigioverse@gmail.com>
2026-07-27 17:51:27 +0500

    Fix phone country menu overlap

--- script.js
+++ script.js
@@ -1014,6 +1014,7 @@ if (document.readyState === 'loading') {
               separateDialCode: true,
               strictMode: true,
               strictRejectAnimation: true,
+              dropdownContainer: document.body,
               countryOrder: ['us', 'ca', 'mx', 'gb'],
               loadUtils: function () {
                 return import('/node_modules/intl-tel-input/dist/js/utils.js');
@@ -1044,7 +1045,13 @@ if (document.readyState === 'loading') {
           phoneCss.setAttribute('data-awad-phone-css', 'true');
           document.head.appendChild(phoneCss);
           var phoneOverrides = document.createElement('style');
-          phoneOverrides.textContent = '.iti{width:100%}.iti input[type="tel"]{width:100%}.iti__country-container{z-index:3}';
+          phoneOverrides.textContent = [
+            '.iti{width:100%;position:relative;z-index:30}',
+            '.iti input[type="tel"]{width:100%}',
+            '.iti__country-container{z-index:31}',
+            '.iti__country-selector{z-index:2147483000!important}',
+            'body>.iti__country-selector{position:absolute!important;z-index:2147483000!important;box-shadow:0 14px 35px rgba(7,21,41,.22)}'
+          ].join('');
           document.head.appendChild(phoneOverrides);
         }
 
